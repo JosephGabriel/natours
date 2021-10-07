a74d9b9d71dfc8ae7cdaff3c4c2b62a4173b2724
JosephGabriel <josegabriel.freelancer@gmail.com>
2021-10-07 17:05:38 -0300

    Padrão MVC aplicado

--- app.js
+++ app.js
@@ -1,62 +1,16 @@
 const express = require('express');
 const app = express();
 
-const fs = require('fs');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tour');
+const userRouter = require('./routes/user');
 
 app.use(express.json());
+app.use(morgan('dev'));
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    resulst: tours.length,
-    data: {
-      tours,
-    },
-  });
-});
-
-app.get('/api/v1/tours/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-});
-
-app.post('/api/v1/tours', (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-});
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 const port = 3000;
 
---
+++ controllers/user.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'Not yet',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'Not yet',
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'Not yet',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'Not yet',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'Not yet',
+  });
+};
