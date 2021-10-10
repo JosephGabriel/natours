431722d8154110c75949374004650b5132b5b767
JosephGabriel <josegabriel.freelancer@gmail.com>
2021-10-10 15:53:34 -0300

    Aplicando estrutura MVC

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tour');
 const userRouter = require('./routes/user');
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-app.use(morgan('dev'));
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log('Banco ok');
+  });
+
+const x = 23;
 
 app.listen(port, () => {
   console.log(`Server on ${port}`);
